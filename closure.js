297005755c69e35bf3f2346b8b8a0570d8d6c524
Alex Young <alex@helicoid.net>
2009-10-23 18:50:49 +0100

    added closure file to repo

---
+++ closure.js
@@ -0,0 +1,49 @@
+var Closure = {
+  VERSION: '0.0.1'
+};
+
+Closure.Class = {
+  create: function() {
+    var methods = null,
+        parent  = undefined;
+
+    if (typeof arguments[0] === 'function') {
+      parent = arguments[0];
+      methods = arguments[1];
+    } else {
+      methods = arguments[0];
+    }
+
+    function klass() { this.initialize.apply(this, arguments); };
+
+    if (typeof parent !== 'undefined')
+      Closure.Class.extend(klass.prototype, parent.prototype);
+
+    Closure.Class.mixin(klass, methods);
+    Closure.Class.extend(klass.prototype, methods);
+    klass.prototype.constructor = klass;
+
+    if (!klass.prototype.initialize)
+      klass.prototype.initialize = function(){};
+
+    return klass;
+  },
+
+  mixin: function(klass, methods) {
+    if (typeof methods.include !== 'undefined') {
+      if (typeof methods.include === 'function') {
+        Closure.Class.extend(klass.prototype, methods.include.prototype);
+      } else {
+        for (var i = 0; i < methods.include.length; i++) {
+          Closure.Class.extend(klass.prototype, methods.include[i].prototype);
+        }
+      }
+    }
+  },
+
+  extend: function(destination, source) {
+    for (var property in source)
+      destination[property] = source[property];
+    return destination;
+  }
+};
